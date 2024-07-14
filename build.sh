0e22bc90b6537bf5d25de650de63e153a88e7759
JALG22018 <lg22018@ues.edu.sv>
2024-07-14 17:51:50 -0600

    update

--- build.sh
+++ build.sh
@@ -2,11 +2,8 @@
 # exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-# pip install -r requirements.txt
+# poetry install
+pip install -r requirements.txt
 
-# Convert static asset files
 python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
 python manage.py migrate
